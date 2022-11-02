8b7cf2e18b24404e675fb4e5aa1d3f2302138ff8
George Bastawros <george.bastawros@outlook.com>
2022-11-01 19:40:33 -0500

    buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
     </div>
-  );
+    <div className="mb-2">
+    <Button variant="primary" size="lg">
+      Large button
+    </Button>{' '}
+    <Button variant="secondary" size="lg">
+      Large button
+    </Button>
+  </div>
+  <div>
+    <Button variant="primary" size="sm">
+      Small button
+    </Button>{' '}
+    <Button variant="secondary" size="sm">
+      Small button
+    </Button>
+  </div>          
+  </>
+ )
+
 }
 
 export default App;
